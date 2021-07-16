483330692737a353b7cad0fba299e3c9538763a4
YFQ <545347049@qq.com>
2021-07-16 16:28:25 +0800

    1.76-16:28

--- data_process/util.py
+++ data_process/util.py
@@ -1,7 +1,7 @@
 # dataset_name: if dataset changed, must to change the dataset_name
 # hf: Heart Failure
 dataset_name = 'cs'
-dataset_path='../../data/'
+dataset_path='../data/'
 # task can be set to 'end' or 'diag'
 task='diag'
 # useful for learning node embedding
